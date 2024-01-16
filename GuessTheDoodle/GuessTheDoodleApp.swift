09c9be10376fe3d503a77988a33dad40ca99e40a
Danyil Marchuk <zemero58@gmail.com>
2024-01-16 18:23:43 +0100

    Initial Commit

---
+++ GuessTheDoodle/GuessTheDoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheDoodleApp.swift
+//  GuessTheDoodle
+//
+//  Created by Данік on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheDoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
