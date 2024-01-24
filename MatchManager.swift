d169737734988119a3f494a35022429d1096074b
Danyil Marchuk <zemero58@gmail.com>
2024-01-24 16:10:41 +0100

    authenticate the users using gameKit

--- MatchManager.swift
+++ MatchManager.swift
@@ -6,16 +6,61 @@
 //
 
 import Foundation
+import GameKit
+import PencilKit
 
 class MatchManager: ObservableObject {
     @Published var inGame = false
     @Published var isGameOver = false
     @Published var authenticationState = PlayerAuthState.authenticating
     
-    @Published var currentlyDrawing = false
+    @Published var currentlyDrawing = true
     @Published var drawPrompt = "Dishwashed"
     @Published var pastGuesses = [PastGuess]()
     
     @Published var score = 0
     @Published var remainingTime = maxTimeRemaining
+    
+    var match: GKMatch?
+    var otherPlayer: GKPlayer?
+    var localPlayer = GKLocalPlayer.local
+    
+    var playerUIIDKey = UUID().uuidString
+    
+    var rootViewController: UIViewController? {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        return windowScene?.windows.first?.rootViewController
+    }
+    
+    func authenticateUser() {
+        GKLocalPlayer.local.authenticateHandler = {[weak self] vc, e in
+            guard let self = self else { return }
+            
+            if let viewController = vc {
+                self.rootViewController?.present(viewController, animated: true)
+                return
+            }
+            
+            if let error = e {
+                authenticationState = .error
+                print(error.localizedDescription)
+                
+                return
+            }
+            
+            if self.localPlayer.isAuthenticated {
+                if self.localPlayer.isMultiplayerGamingRestricted {
+                    self.authenticationState = .restricted
+                } else {
+                    self.authenticationState = .authenticated
+                }
+                
+            } else {
+                self.authenticationState = .unauthenticating
+            }
+        }
+    }
+    
+    
+    
 }
